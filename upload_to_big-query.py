eed9f91eb6776166178034f1269a93b0a8af635d
fingers53 <54557739+fingers53@users.noreply.github.com>
2022-12-08 04:17:43 +0100

    Scraping every candidate ever

--- upload_to_big-query.py
+++ upload_to_big-query.py
@@ -1,5 +1,4 @@
 import pandas as pd
-# import pandas_gbq
 from google.cloud import bigquery
 
 GOOGLE_APPLICATION_CREDENTIALS = "secrets/encoded-axis-369815-759553ed797a.json"
@@ -7,14 +6,14 @@ client = bigquery.Client.from_service_account_json(
     GOOGLE_APPLICATION_CREDENTIALS)
 
 
-table_id = 'encoded-axis-369815.SCRAPED_IRISH_ELECTIONS.dail_elections'
+table_id = 'encoded-axis-369815.SCRAPED_IRISH_ELECTIONS.All_Elections_Every_Candidate'
 
 #table = bigquery.Table(table_id)
 # table = client.create_table(table)  # Make an API request.
 #print(f"Created table {table.project} {table.dataset_id} {table.table_id}")
 
-DF = pd.read_parquet('DAIL_elections_master.parquet')
+DF = pd.read_parquet('ALL_CANDIDATES.parquet')
 print(DF)
 
 job = client.load_table_from_dataframe(DF, table_id)
-job.result()
+print(job.result())
