77c0332c4dfa0a2926c0b831e2f6c320b12ab794
iggy002 <56845156+iggy002@users.noreply.github.com>
2022-12-15 14:48:23 +0000

    added a .py file

---
+++ TEST_FILES/record_linkage.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import datetime as dt
+import functools
+from os import listdir
+from os.path import isfile, join
+import pathlib
+
+# Libs 
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+from pandas.io import gbq
+from functools import wraps
+
+from functools import wraps
+from google.oauth2 import service_account
+#import pandas_gbq
+
+
+
+# dealing with non relative paths
+import pathlib
+from pathlib import Path
+
+
+
+"""
+Tables we want : 
+    Number of total elections ? 
+    Number of total candaites ? 
+    Coverage per website i.e. how many elections does each site have
+        - How many people ran per election 
+        - How many 
+            -> constituencies
+            -> how many elected reps 
+            -> how many reps 
+"""
+
+# Files # 
+file_name_irelandelection = "C:/Users/fenlo/work repos/Scraping_Dail_results/irelandelection/ALL_CANDIDATES.parquet"
+file_name_electionsireland = "C:/Users/fenlo/work repos/Scraping_Dail_results/electionsireland_data/ElectionsIreland_candidate.parquet"
+
+
+df_irelandelection = pd.read_parquet(file_name_irelandelection)
+df_electionsireland = pd.read_parquet(file_name_electionsireland)
+
+import recordlinkage
