a1b7e3d3602e38e0b9b3044fe9f4b2ad76ac8c52
fingers53 <54557739+fingers53@users.noreply.github.com>
2022-12-15 13:14:04 +0000

    mid point, for Matthew

--- upload_to_big-query.py
+++ upload_to_big-query.py
@@ -6,14 +6,19 @@ client = bigquery.Client.from_service_account_json(
     GOOGLE_APPLICATION_CREDENTIALS)
 
 
-table_id = 'encoded-axis-369815.SCRAPED_IRISH_ELECTIONS.All_Elections_Every_Candidate'
+table_id = 'red-bus-371614.Testing.All_Elections_Every_Candidate'
 
-#table = bigquery.Table(table_id)
-# table = client.create_table(table)  # Make an API request.
-#print(f"Created table {table.project} {table.dataset_id} {table.table_id}")
+table = bigquery.Table(table_id)
+table = client.create_table(table)  # Make an API request.
+print(f"Created table {table.project} {table.dataset_id} {table.table_id}")
 
-DF = pd.read_parquet('ALL_CANDIDATES.parquet')
+job_config = bigquery.LoadJobConfig(
+    write_disposition="WRITE_TRUNCATE",
+)
+
+
+DF = pd.read_parquet('irelandelection/ALL_CANDIDATES.parquet')
 print(DF)
 
-job = client.load_table_from_dataframe(DF, table_id)
+job = client.load_table_from_dataframe(DF, table_id, job_config=job_config)
 print(job.result())
