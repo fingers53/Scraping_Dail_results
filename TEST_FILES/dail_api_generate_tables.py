5cfa0e6617e9aaa4d13617a5f57ba919c5492775
iggy002 <56845156+iggy002@users.noreply.github.com>
2022-12-29 16:18:01 +0000

    finished script that pulls data from dail api. The script generates 3 tables that will be used for cleaning the election result data.

--- TEST_FILES/dail_api_generate_tables.py
+++ TEST_FILES/dail_api_generate_tables.py
@@ -66,26 +66,26 @@ def upload(dataframe,destination_table,what_to_if_table_exists,project_id,creden
 
     
 ##############################################################################
-def get_dail_constituencies_all():
+@logg
+def get_dail_constituencies_all(latest_dail : int):
     
     # stores data #
     data_dail_constituencies = []
     
     # Dails to itterate through 
-    latest_dail = 33
-    lst = [str(i) for i in range(1, latest_dail+1)]
+    latest_dail = latest_dail
     
     # All constituencies # 
     headers = {
         'accept': 'application/json',
     }
     
-    for i in lst : 
+    for i in range(1, latest_dail+1) : 
             
         params = {
             'chamber_type': 'house',
             'chamber': 'dail',
-            'house_no': i,
+            'house_no': str(i),
             'memberCode':"" ,
             
             'limit': '1000',
@@ -114,142 +114,202 @@ def get_dail_constituencies_all():
                     })
     
     
-    # 
+    # final dataframe
     df_dail_constituencies = pd.DataFrame(data_dail_constituencies)
     
     
+    # used in 
+    project_id= "red-bus-371614"        
+    credentials = service_account.Credentials.from_service_account_file('./../secrets/big_key.json')
+    
+    destination_table = 'oireachtas_api.dail_constituencies'
+    what_to_if_table_exists = 'replace' 
+    
+    upload(df_dail_constituencies,destination_table,'replace',project_id,credentials)# uploades dataframe to bigquery
+
     
     return df_dail_constituencies
 
-
-#df_dail_constituencies = get_dail_constituencies_all()
-
-"""
-# used in 
-project_id= config['IE']['project_id']         
-credentials = service_account.Credentials.from_service_account_file('../../secrets/big_key.json')
-
-dataset = department
-destination_table = str(dataset)+ '.' + str(config['IE']['Tables'][department][report_dataset]['BIGQUERY_TABLE']) # syntaca : data_set.table_name
-what_to_if_table_exists = 'replace' 
-
-upload(data_frame_spreadsheet_info,destination_table,'replace',project_id,credentials)# uploades dataframe to bigquery
-"""
-
-"""
-Stats I want 
- - Number of constituencies per dail 
-"""
-# All reps # 
- # stores data #
-data_dail_members = [] # more than the number of seats, due to bye elections
-data_dail_number_of_members = [] # will have more deputies than this due to bye elections???
-data_dail_members_changed_party = [] # more than the number of seats, due to bye elections
-
-# Dails to itterate through 
-latest_dail = 33
-lst = [str(i) for i in range(1, latest_dail+1)]
-
-headers = {
-    'accept': 'application/json',
-}
-for i in lst: 
+@logg
+def get_dail_members_all(latest_dail : int) :
     
-    params = {
-        'chamber': 'dail',
-        'house_no': i,
-        'limit': '1000',
+    # All reps # 
+     # stores data #
+    data_dail_members = [] # more than the number of seats, due to bye elections
+    data_dail_number_of_members = [] # will have more deputies than this due to bye elections???
+    
+    # Dails to itterate through 
+    latest_dail = latest_dail
+    
+    headers = {
+        'accept': 'application/json',
     }
     
-    response = requests.get('https://api.oireachtas.ie/v1/members', params=params, headers=headers).json()
-    
-    # Total number of memebers per dail 
-    total_memebers_per_dail = response["head"]['counts']['memberCount']
-    data_dail_number_of_members.append({
-                    'houseCode' : 'dail',
-                    'house_no' : i, # the value being itterateed
-                    'memberCount': total_memebers_per_dail,
-                    
-                        })
-    
-    # The memebers that make up each dail 
-    memebers_of_dail  = response['results']
-   
-     
-    for data in memebers_of_dail:  # data is a dict of size 10 
-          for info in data : 
-              
-              parties = data[info]['memberships'][0]['membership']['parties']
-              parties_list = []
-              for i in parties:
-                  parties_list.append(i)
-              
-              data_dail_members.append({
-                  'memberCode': data[info]['memberCode'],
-                  'firstName': data[info]['firstName'] ,
-                  'lastName': data[info]['lastName'], 
-                  'gender': data[info]['gender'], 
-                  'dateOfDeath': data[info]['dateOfDeath'], 
-                  'fullName': data[info]['fullName'], 
-                  'pId': data[info]['pId'], 
-                  'wikiTitle': data[info]['wikiTitle'], 
-                  'uri': data[info]['uri'], 
-                  'memberships': data[info]['memberships'][0]['membership'],
-                  'houseCode': data[info]['memberships'][0]['membership']['house']['houseCode'],
-                  'houseNo': data[info]['memberships'][0]['membership']['house']['houseNo'],
-                  'start': data[info]['memberships'][0]['membership']['dateRange']['start'],
-                  'end' : data[info]['memberships'][0]['membership']['dateRange']['end'],
-                  'parties' : parties_list[0] , #data[info]['memberships'][0]['membership']['parties'],
-                  'represents' : data[info]['memberships'][0]['membership']['represents'],
-                  'representCode': data[info]['memberships'][0]['membership']['represents'][0]['represent']['representCode'],
-                  'name_2nd': data[info]['memberships'][0]['membership']['represents'][0]['represent']['showAs'],
-                  'uri_represents': data[info]['memberships'][0]['membership']['represents'][0]['represent']['uri'],
-               
-                 })
-              
+    for i in range(1, latest_dail+1) : 
+        
+        params = {
+            'chamber': 'dail',
+            'house_no': str(i),
+            'limit': '1000',
+        }
+        
+        response = requests.get('https://api.oireachtas.ie/v1/members', params=params, headers=headers).json()
+        
+        # Total number of memebers per dail 
+        total_memebers_per_dail = response["head"]['counts']['memberCount']
+        data_dail_number_of_members.append({
+                        'houseCode' : 'dail',
+                        'house_no' : i, # the value being itterateed
+                        'memberCount': total_memebers_per_dail,
+                        
+                            })
+        
+        # The memebers that make up each dail 
+        memebers_of_dail  = response['results']
+       
+         
+        for data in memebers_of_dail:  # data is a dict of size 10 
+              for info in data : 
                   
+                  if len(data[info]['memberships'][0]['membership']['represents']) == 0 :
+                      represents = ""
+                      representCode = ""
+                      name_2nd = ""
+                      uri_represents = "" 
+                  else : 
+                      represents = data[info]['memberships'][0]['membership']['represents']
+                      representCode = data[info]['memberships'][0]['membership']['represents'][0]['represent']['representCode']
+                      name_2nd = data[info]['memberships'][0]['membership']['represents'][0]['represent']['showAs']
+                      uri_represents = data[info]['memberships'][0]['membership']['represents'][0]['represent']['uri']
+                   
                   
-              if len(parties_list) != 1 : 
-                  print("yes")
-                  print(len(parties_list))
-                  
-                  data_dail_members_changed_party.append({
-                  'memberCode': data[info]['memberCode'],
-                  'firstName': data[info]['firstName'] ,
-                  'lastName': data[info]['lastName'], 
-                  'gender': data[info]['gender'], 
-                  'dateOfDeath': data[info]['dateOfDeath'], 
-                  'fullName': data[info]['fullName'], 
-                  'pId': data[info]['pId'], 
-                  'wikiTitle': data[info]['wikiTitle'], 
-                  'uri': data[info]['uri'], 
-                  'memberships': data[info]['memberships'][0]['membership'],
-                  'houseCode': data[info]['memberships'][0]['membership']['house']['houseCode'],
-                  'houseNo': data[info]['memberships'][0]['membership']['house']['houseNo'],
-                  'start': data[info]['memberships'][0]['membership']['dateRange']['start'],
-                  'end' : data[info]['memberships'][0]['membership']['dateRange']['end'],
-                  'parties' : parties_list[1] , #data[info]['memberships'][0]['membership']['parties'],
-                  'represents' : data[info]['memberships'][0]['membership']['represents'],
-                  'representCode': data[info]['memberships'][0]['membership']['represents'][0]['represent']['representCode'],
-                  'name_2nd': data[info]['memberships'][0]['membership']['represents'][0]['represent']['showAs'],
-                  'uri_represents': data[info]['memberships'][0]['membership']['represents'][0]['represent']['uri'],
-                  'number_of_parties':parties_list,
-                 })
-                 
-              else : 
-                  pass
-              
+                  data_dail_members.append({
+                      'memberCode': data[info]['memberCode'],
+                      'firstName': data[info]['firstName'] ,
+                      'lastName': data[info]['lastName'], 
+                      'gender': data[info]['gender'], 
+                      'dateOfDeath': data[info]['dateOfDeath'], 
+                      'fullName': data[info]['fullName'], 
+                      'pId': data[info]['pId'], 
+                      'wikiTitle': data[info]['wikiTitle'], 
+                      'uri': data[info]['uri'], 
+                      'memberships': data[info]['memberships'][0]['membership'],
+                      'houseCode': data[info]['memberships'][0]['membership']['house']['houseCode'],
+                      'houseNo': data[info]['memberships'][0]['membership']['house']['houseNo'],
+                      'start': data[info]['memberships'][0]['membership']['dateRange']['start'],
+                      'end' : data[info]['memberships'][0]['membership']['dateRange']['end'],
+                      'represents' : represents,
+                      'representCode': representCode,
+                      'name_2nd': name_2nd,
+                      'uri_represents': uri_represents,
+                   
+                     })
 
+
+        
+    df_data_dail_members = (pd.DataFrame(data_dail_members)
+                                    .drop(columns=['represents','memberships'])
+                                    .rename(columns={'start': 'start_of_term', 
+                                                       'end': 'end_of_term'})
+                            )
+    df_data_dail_number_of_members = pd.DataFrame(data_dail_number_of_members)
     
-df_data_dail_members = pd.DataFrame(data_dail_members)
-df_data_dail_number_of_members = pd.DataFrame(data_dail_number_of_members)
 
-
-
-
-
-def get_dail_members_all():
+    # used in 
+    project_id= "red-bus-371614"        
+    credentials = service_account.Credentials.from_service_account_file('./../secrets/big_key.json')
     
+    destination_table = 'oireachtas_api.dail_members'
+    what_to_if_table_exists = 'replace' 
     
+    upload(df_data_dail_members,destination_table,'replace',project_id,credentials)# uploades dataframe to bigquery
+
+    return df_data_dail_members
+
+@logg
+def get_dail_members_party_history(latest_dail : int):
+        
+    #All reps # 
+    # stores data #
+    data_dail_members = [] # more than the number of seats, due to bye elections
+    data_dail_number_of_members = [] # will have more deputies than this due to bye elections???
+    list_parties = []         # for tracking parties
+
+    party_data = []
+
+    # Dails to itterate through 
+    latest_dail = latest_dail
     
-    return df_dail_constituencies
+    headers = {
+        'accept': 'application/json',
+    }
+    
+    for i in range(1, latest_dail+1) : 
+        
+        params = {
+            'chamber': 'dail',
+            'house_no': str(i),
+            'limit': '1000',
+        }
+        
+        response = requests.get('https://api.oireachtas.ie/v1/members', params=params, headers=headers).json()
+        
+        # Total number of memebers per dail 
+        total_memebers_per_dail = response["head"]['counts']['memberCount']
+        data_dail_number_of_members.append({
+                        'houseCode' : 'dail',
+                        'house_no' : i, # the value being itterateed
+                        'memberCount': total_memebers_per_dail,
+                        
+                            })
+        
+        # The memebers that make up each dail 
+        memebers_of_dail  = response['results']
+       
+
+        house_no = i
+        
+        for data in memebers_of_dail: 
+            # data is dict withing the list memebers_of_dail
+            # each dict is linked to a member of the dail for a given dail term
+            for info in data : 
+            # info is the info in the data dict 
+                member = data[info]['memberCode']
+                houseCode = 'dail'
+                memberships_info = data[info]['memberships'][0]['membership'] # party data
+                
+                for membership in memberships_info:
+                    # Extract the parties list from the membership
+                    parties = memberships_info['parties']
+                    # Loop through the parties
+                    for party in parties:
+                        # Extract the party name and date range
+                        party_name = party['party']['showAs']
+                        date_range = party['party']['dateRange']
+            
+                        party_data.append((party_name, date_range,member,houseCode,house_no))
+                        
+                              
+        
+    df = pd.DataFrame(party_data, columns=['party', 'date_range','member',"houseCode","house_no"])
+    df[['start', 'end']] = df['date_range'].apply(lambda x: pd.Series({'start': x['start'], 'end': x['end']}))
+    
+    # Dataframe to be uploaded 
+    df = df[['party', 'member', 'start', 'end',"houseCode","house_no"]].drop_duplicates()
+        
+     # used in 
+    project_id= "red-bus-371614"        
+    credentials = service_account.Credentials.from_service_account_file('./../secrets/big_key.json')
+    
+    destination_table = 'oireachtas_api.dail_members_party_history'
+    what_to_if_table_exists = 'replace' 
+    
+    upload(df,destination_table,'replace',project_id,credentials)# uploades dataframe to bigquery
+
+    return df
+
+if __name__ == "__main__":
+    a = get_dail_constituencies_all(33)
+    aa = get_dail_members_all(33)
+    aaa = get_dail_members_party_history(33)
+    
--- TEST_FILES/record_linkage.py
+++ TEST_FILES/record_linkage.py
@@ -23,52 +23,87 @@ from google.oauth2 import service_account
 import pathlib
 from pathlib import Path
 
+# Base 
+def logg(func):
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		tic = dt.datetime.now()
+		result = func(*args, **kwargs)
+		time_taken = str(dt.datetime.now() - tic)
+		try:
+			print(f"just ran step {func.__name__} shape={result.shape} took {time_taken}s")
+		except:
+			print(f"just ran step {func.__name__} took {time_taken}s")
+		return result
+	return wrapper
+
 
 # Functions 
-def get_year_from_date_string(date_str):
-    if date_str == None:
-        return 0
-    elif len(date_str) > 4:
-        try:
-            return int(date_str[-4:])
-        except:
-            if isinstance(date_str[-1],int):#last letter is an int
-                return date_str[-4:]
-    else:
-        return int(date_str)
+ 
+@logg
+def get_election_type(data,colum):
+    
+    def get_election_type_from_string(election_type_str):
+        if election_type_str is None:
+            return None
+        elif 'Town' in election_type_str or 'Local' in election_type_str :
+            return 'LOCAL'
+        elif 'Dail' in election_type_str:
+            return 'GENERAL'
+        elif 'Seanad' in election_type_str:
+            return 'SEANAD'
+        elif 'Westminster' in election_type_str:
+            return 'Westminster'.upper()
+        elif 'European' in election_type_str:
+            return 'EUROPEAN'
+        elif 'By Election' in election_type_str:
+            return 'BI-ELECTION'
+        else: # for the rows that represent resignations or appointments or some other event in a politicans career
+            return None
+        
+    data[str(colum)] = data['election_type'].apply(get_election_type_from_string)
+    
+    return data
+  
+@logg
+def was_elected(data,colum):
+    
+    def check_status(status):
+        if status == 'Elected':
+            return True
+        elif status == 'Not Elected':
+            return False
+        else: # for the rows that represent resignations or appointments or some other event in a politicans career
+            return None
+    
+    # create column
+    data[str(colum)] = data['status'].apply(check_status)
+    
+    return data
 
-def get_election_type_from_string(election_type_str):
-    if 'Town' in election_type_str or 'Local' in election_type_str :
-        return 'LOCAL'
-    elif 'Dail' in election_type_str:
-        return 'GENERAL'
-    elif 'Seanad' in election_type_str:
-        return 'SEANAD'
-    elif 'Westminster' in election_type_str:
-        return 'Westminster'.upper()
-    elif 'European' in election_type_str:
-        return 'EUROPEAN'
-    elif 'By Election' in election_type_str:
-        return 'BI-ELECTION'
-    else: # for the rows that represent resignations or appointments or some other event in a politicans career
-        return None
-
-def was_elected(status):
-    status = str(status) # some of the status are None 
-
-    if status == 'Elected':
-        return True
-    elif status == 'Not Elected':
-        return False
-
-    else: # for the rows that represent resignations or appointments or some other event in a politicans career
-        return None
-
-def fix_party(main,column_name) : 
+@logg
+def get_year_from_date_string(main):
     
     
-    
-    return 
+    # couldnt work how to get this to work without using apply
+    def get_year_from_date_string_1(date_str):
+        if date_str == None:
+            return 0
+        elif len(date_str) > 4:
+            try:
+                return int(date_str[-4:])
+            except:
+                if isinstance(date_str[-1],int):#last letter is an int
+                    return date_str[-4:]
+        else:
+            return int(date_str)
+        
+    main['year'] = (main
+                   .date
+                   .apply(get_year_from_date_string_1)
+                   )
+
+    return main
     
 """
 Tables we want : 
@@ -87,41 +122,30 @@ file_name_irelandelection = "../irelandelection/ALL_CANDIDATES.parquet"
 file_name_electionsireland = '../electionsireland_data/ElectionsIreland_candidate.parquet'
 
 
+# df_electionsirelan 
+# Cleaning # 
+# 1. Rename columns : rename(columns={'ID':'candidate_ID'})
+# 2. Drop columns : .drop(columns=['seat','count_eliminated'])
+# 3. get_year_from_date_string : date.apply(get_year_from_date_string)
 
 df_electionsireland = (pd.read_parquet(file_name_electionsireland)
                        .rename(columns={'ID':'candidate_ID'}) # rename columns
                        .drop(columns=['seat','count_eliminated']) # drop columns
                        .reset_index() # reset index
                        .drop(columns=['index']) # drop column
-                       #.pipe(get_year_from_date_string,'year') one day would have this logic in a function that can use .pipe
+                       .pipe(get_year_from_date_string) # get year of election
+                       .drop(columns=['date'])  # drop column
+                       .pipe(was_elected,"elected") # create column that returns True or False if someone was elected or not
+                       .pipe(get_election_type,"election_type") # extract the election type 
                        )
 
-df_electionsireland['year'] = (df_electionsireland
-                               .date
-                               .apply(get_year_from_date_string)
-                               )
-df_electionsireland = df_electionsireland.drop(columns=['date']) # drop column
-df_electionsireland['election_type'] = (df_electionsireland
-                                        .election_type
-                                        .apply(get_election_type_from_string)
-                                        )
-df_electionsireland['elected'] = (df_electionsireland
-                                  .status
-                                  .apply(was_elected)
-                                  )
-
-
-# Ground truth
-historic_constituencies = pd.read_html('https://en.wikipedia.org/wiki/Historic_D%C3%A1il_constituencies',flavor='bs4')[1]
-current_constituencies = pd.read_html('https://en.wikipedia.org/wiki/D%C3%A1il_constituencies',flavor='bs4')[3]
-
-historic_elections = pd.read_html('https://en.wikipedia.org/wiki/Elections_in_the_Republic_of_Ireland',flavor='bs4')[3]
 
 
 
+# df_irelandelection
+# Cleaning # 
+# 1. Rename columns : .rename( columns={'first_pref_quota_ratio':'pct_of_quota_reached_with_first_pref', 'constituency':'constituency_name'} )
 
-
-"""
 df_irelandelection = (pd.read_parquet(file_name_irelandelection)
                       .rename(
                          columns={'first_pref_quota_ratio':'pct_of_quota_reached_with_first_pref',
@@ -130,4 +154,6 @@ df_irelandelection = (pd.read_parquet(file_name_irelandelection)
                       .reset_index()
                       .drop(columns=['index'])
                       )
-"""
+
+
+
